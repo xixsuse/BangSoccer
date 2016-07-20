bf28af92a3f90af5af364f0424a1769043aad422
frO <jfrometa@gmail.com>
2016-07-20 19:54:02 -0400

    integrated with newsfeed updates, missing email auth : 70%

--- app/src/main/java/upgrade/ntv/bangsoccer/AppicationCore.java
+++ app/src/main/java/upgrade/ntv/bangsoccer/AppicationCore.java
@@ -2,12 +2,16 @@ package upgrade.ntv.bangsoccer;
 
 import android.app.Application;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.List;
 
@@ -36,9 +40,16 @@ import static upgrade.ntv.bangsoccer.AppConstants.AppConstant.populateDummyClubs
 
 public class AppicationCore extends Application {
 
-    public static DatabaseReference databaseReference ;
-    public  static DatabaseReference mPlayersDeftailsRef ;
-    public  static DatabaseReference mTeamsRef ;
+    private DatabaseReference databaseReference;
+    public static DatabaseReference mPlayersDeftailsRef;
+    public static DatabaseReference mTeamsRef;
+    private StorageReference storageReference;
+    public static StorageReference mPrimeraRef;
+    public static StorageReference mSegundaRef;
+    public static StorageReference mTerceraRef;
+    public static StorageReference mCuartaRef;
+    public static FirebaseAuth authReference;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
 
     //GreenDao variables
     private static DaoMaster daoMaster;
@@ -66,27 +77,47 @@ public class AppicationCore extends Application {
         });
 */
 
-        if (databaseReference == null){
-            try{
+        if (databaseReference == null) {
+            try {
                 if (!FirebaseApp.getApps(this).isEmpty()) {
                     FirebaseDatabase.getInstance().setPersistenceEnabled(true);
                 }
-            }catch (Exception e) {
-              //  FirebaseCrash.log("firebase Crash reports  failed to initialize");
+            } catch (Exception e) {
+                //  FirebaseCrash.log("firebase Crash reports  failed to initialize");
                 Log.i("firebase persistance: ", e.getMessage());
-            }finally
-             {
+            } finally {
+               // FirebaseStorage storage = FirebaseStorage.getInstance();
+                storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://project-8138681142864808550.appspot.com/Test_MediaCancha/");
+                mPrimeraRef = storageReference.child("primera");
+                mSegundaRef = storageReference.child("segunda");
+                mTerceraRef = storageReference.child("tercera");
+                mCuartaRef = storageReference.child("cuarta");
                 databaseReference = FirebaseDatabase.getInstance().getReference();
                 mPlayersDeftailsRef = databaseReference.child("Players");
                 mTeamsRef = databaseReference.child("Team");
+                authReference =FirebaseAuth.getInstance();
+                mAuthListener = new FirebaseAuth.AuthStateListener() {
+                    @Override
+                    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        if (user != null) {
+                            // User is signed in
+                            Log.d("AUTH", "onAuthStateChanged:signed_in:" + user.getUid());
+                        } else {
+                            // User is signed out
+                            Log.d("AUTH", "onAuthStateChanged:signed_out");
+                        }
+                        // ...
+                    }
+                };authReference.addAuthStateListener(mAuthListener);
             }
         }
 
         populateDummyClubsItems();
 
 
-    // Database Initialization
-        DaoMaster.DevOpenHelper helper  = new DaoMaster.DevOpenHelper(this, "soccerDB", null);
+        // Database Initialization
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "soccerDB", null);
         db = helper.getWritableDatabase();
         daoMaster = new DaoMaster(db);
         DaoSession daoSession = daoMaster.newSession();  // to avoid memory leaks
@@ -106,48 +137,47 @@ public class AppicationCore extends Application {
     /**
      * Reset all values stored in the db
      */
-    public static void resetDB(){
+    public static void resetDB() {
         daoMaster.dropAllTables(db, true);
-        daoMaster.createAllTables(db, false);;
+        daoMaster.createAllTables(db, false);
+        ;
     }
 
     /**
      * Reset values from a NewsFeed table
      */
-    public static void resetNewsFeedTable(){
+    public static void resetNewsFeedTable() {
         dbNewsFeedDao.deleteAll();
     }
 
 
-
-
 // **************** Get Dao methods == newInstance ****************
 
-    public static DBClubDao getDbClubDao(){
+    public static DBClubDao getDbClubDao() {
         return dbClubDao;
     }
 
-    public static DBTourneyDao getDbTourneyDao(){
+    public static DBTourneyDao getDbTourneyDao() {
         return dbTourneyDao;
     }
 
-    public static DBTeamDao getDbTeamDao(){
+    public static DBTeamDao getDbTeamDao() {
         return dbTeamDao;
     }
 
-    public static DBMatchDao getDbMatchDao(){
+    public static DBMatchDao getDbMatchDao() {
         return dbMatchDao;
     }
 
-    public static DBPlayerDao getDbPlayerDao(){
+    public static DBPlayerDao getDbPlayerDao() {
         return dbPlayerDao;
     }
 
-    public static DBMatchesPlayerDetailsDao getDbMatchesPlayerDetailsDao(){
+    public static DBMatchesPlayerDetailsDao getDbMatchesPlayerDetailsDao() {
         return dbMatchesPlayerDetailsDao;
     }
 
-    public static DBNewsFeedDao getDbNewsFeedDao(){
+    public static DBNewsFeedDao getDbNewsFeedDao() {
         return dbNewsFeedDao;
     }
 
@@ -156,38 +186,42 @@ public class AppicationCore extends Application {
 
     /**
      * Get all the clubs in te db
+     *
      * @return a list of clubs
      */
-    public static List<DBClub> getClubs(){
+    public static List<DBClub> getClubs() {
         return dbClubDao.loadAll();
     }
 
     /**
      * Get a club based on the ID
+     *
      * @param clubID
      * @return a DBClub object
      */
-    public static DBClub getClub(long clubID){
-        return  dbClubDao.load(clubID);
+    public static DBClub getClub(long clubID) {
+        return dbClubDao.load(clubID);
     }
 
     /**
      * Get club based on team ID
+     *
      * @param teamID
      * @return
      */
-    public static DBClub getClubByTeamID(long teamID){
+    public static DBClub getClubByTeamID(long teamID) {
 
-    DBTeam team = dbTeamDao.load(teamID);
-    return  dbClubDao.load(team.getClubID());
+        DBTeam team = dbTeamDao.load(teamID);
+        return dbClubDao.load(team.getClubID());
     }
 
     /**
      * Get all the teams under a specific club
+     *
      * @param clubID
      * @return a list of teams
      */
-    public static List<DBTeam> getTeams (long clubID) {
+    public static List<DBTeam> getTeams(long clubID) {
 
         QueryBuilder<DBTeam> queryBuilder = dbTeamDao.queryBuilder();
         queryBuilder.where(DBTeamDao.Properties.ClubID.eq(clubID));
@@ -197,19 +231,21 @@ public class AppicationCore extends Application {
 
     /**
      * Get a team based on the ID
+     *
      * @param teamID
      * @return a DBTeam object
      */
-    public static DBTeam getTeam (long teamID){
+    public static DBTeam getTeam(long teamID) {
         return dbTeamDao.load(teamID);
     }
 
     /**
      * Get all the players under a specific team
+     *
      * @param teamID
      * @return a list of players
      */
-    public static List<DBPlayer> getPlayers (long teamID){
+    public static List<DBPlayer> getPlayers(long teamID) {
 
         QueryBuilder<DBPlayer> queryBuilder = dbPlayerDao.queryBuilder();
         queryBuilder.where(DBPlayerDao.Properties.TeamID.eq(teamID));
@@ -219,19 +255,21 @@ public class AppicationCore extends Application {
 
     /**
      * Get a player based on the ID
+     *
      * @param playerID
      * @return a DBPlayer object
      */
-    public static DBPlayer getPlayer(long playerID){
+    public static DBPlayer getPlayer(long playerID) {
         return dbPlayerDao.load(playerID);
     }
 
 
     /**
      * Get all the tourneys that are active
+     *
      * @return a list of tourneys
      */
-    public static List<DBTourney> getActiveTourneys(){
+    public static List<DBTourney> getActiveTourneys() {
 
         QueryBuilder<DBTourney> queryBuilder = dbTourneyDao.queryBuilder();
         queryBuilder.where(DBTourneyDao.Properties.Active.eq(true));
@@ -242,18 +280,20 @@ public class AppicationCore extends Application {
 
     /**
      * Get a tourney based on the ID
+     *
      * @param tourneyID
      * @return a DBTourney object
      */
-    public static DBTourney getTourney(long tourneyID){
+    public static DBTourney getTourney(long tourneyID) {
         return dbTourneyDao.load(tourneyID);
     }
 
     /**
      * Get all the matches with same WeeklyScheduleID
+     *
      * @return a list of matches
      */
-    public static List<DBMatch> getMatches(int WeeklyScheduleID ){
+    public static List<DBMatch> getMatches(int WeeklyScheduleID) {
 
         QueryBuilder<DBMatch> queryBuilder = dbMatchDao.queryBuilder();
         queryBuilder.where(DBMatchDao.Properties.Week.eq(WeeklyScheduleID));
@@ -263,32 +303,32 @@ public class AppicationCore extends Application {
 
     /**
      * Get all the matches
+     *
      * @return a list of matches
      */
-    public static List<DBMatch> getMatches(){
+    public static List<DBMatch> getMatches() {
         return dbMatchDao.loadAll();
     }
 
 
     /**
      * Get all the News Feed in te db
+     *
      * @return a list of NewsFeed
      */
-    public static List<DBNewsFeed> getAllNewsFeed(){
+    public static List<DBNewsFeed> getAllNewsFeed() {
         return dbNewsFeedDao.loadAll();
     }
 
     /**
      * Get a club based on the ID
+     *
      * @param newsFeedID
      * @return a DBClub object
      */
-    public static DBNewsFeed getNewsFeed(long newsFeedID){
-        return  dbNewsFeedDao.load(newsFeedID);
+    public static DBNewsFeed getNewsFeed(long newsFeedID) {
+        return dbNewsFeedDao.load(newsFeedID);
     }
-
-
-
 
 
 }

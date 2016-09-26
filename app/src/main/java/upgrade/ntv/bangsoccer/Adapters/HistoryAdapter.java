0c1106fbc978f55e11e3ba037f435872928a63da
Paulino Gomez <xeroshock@gmail.com>
2016-09-26 19:11:59 -0400

    Adding History

--- app/src/main/java/upgrade/ntv/bangsoccer/Adapters/HistoryAdapter.java
+++ app/src/main/java/upgrade/ntv/bangsoccer/Adapters/HistoryAdapter.java
@@ -1,142 +1,160 @@
 package upgrade.ntv.bangsoccer.Adapters;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import upgrade.ntv.bangsoccer.AppConstants.Constants;
-import upgrade.ntv.bangsoccer.Drawer.DrawerSelector;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import upgrade.ntv.bangsoccer.ActivityMain;
 import upgrade.ntv.bangsoccer.R;
+import upgrade.ntv.bangsoccer.TournamentObjects.History;
 import upgrade.ntv.bangsoccer.TournamentObjects.Match;
 
 /**
  * Created by jfrom on 3/19/2016.
  */
-public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ScheduleHolder> {
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryMatchHolder>
+        implements View.OnClickListener{
 
-    private List<Match> mClubsMatches = new ArrayList<>();
-    private Context mContext;
+    private List<Match> mMatchesHistory = new ArrayList<>();
 
+    private Context
+            mContext;
 
-    public HistoryAdapter(Context context) {
+    private String
+            mClubID,
+            mDivision;
 
+    public HistoryAdapter(Context context, String clubId, String division) {
+
+        this.mClubID = clubId;
+        this.mDivision = division;
         this.mContext = context;
+
+        Query query = ActivityMain.databaseReference.child(division + "_History").child(clubId);
+        query.addValueEventListener(new HistoryEventListener());
 
     }
 
+    //firebase event listener
+    private class HistoryEventListener implements ValueEventListener {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // whenever data at this location is updated.
+            for (DataSnapshot mdSnapshot : dataSnapshot.getChildren()) {
+
+                History history = mdSnapshot.getValue(History.class);
+                history.setHISTORY_ID(dataSnapshot.getKey());
+
+                Query query = ActivityMain.databaseReference
+                        .child(mDivision + "_Calendar")
+                        .child(history.getDate())
+                        .child(history.getVS());
+                query.addValueEventListener(new MatchEventListener());
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    }
+
+    //firebase event listener
+    private class MatchEventListener implements ValueEventListener {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            Match match = dataSnapshot.getValue(Match.class);
+            match.setMatchId(dataSnapshot.getKey());
+            mMatchesHistory.add(match);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    }
 
     @Override
     public int getItemCount() {
-        return (this.mClubsMatches.size());
+        return (this.mMatchesHistory.size());
     }
 
     @Override
-    public ScheduleHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public HistoryMatchHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.row_history_match_v2, parent, false);
-        // set the view's size, margins, paddings and app_bar_teams parameters
 
-        return new ScheduleHolder(v);
+        return new HistoryMatchHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(ScheduleHolder holder, int position) {
-        // - get element from your dataset at this vPlayerPosition
-        // - replace the contents of the view with that element
-        String result = String.valueOf(5 + (int) (Math.random() * ((5) + 1)));
-        final  int pos = position;/*
-        holder.teamName1.setText(mClubsMatches.getmWeeklyMatch().get(position).getTeamName(1));
-        holder.teamName2.setText(mClubsMatches.getmWeeklyMatch().get(position).getTeamName(2));*/
-        Picasso.with(mContext).
-                load(mClubsMatches.get(pos).getClubIdA().getTeam_image()).
-                placeholder(R.drawable.ic_upgraden).
-                into(holder.teamLogo1);
-        //holder.teamLogo1.setImageResource(mClubsMatches.getmWeeklyMatch().get(position).getTeamImage(1));
-        holder.teamLogo1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //calls the Team1 Screen based on the teamID
-                //TODO: replace with local DBSource
-                Intent intent = DrawerSelector.onItemSelected((Activity) mContext, Constants.CLUBS_ACTIVITY_BY_TEAM);
-                intent.putExtra("CLUBID", mClubsMatches.get(pos).getClubIdA().getFirebasekey());
+    public void onBindViewHolder(HistoryAdapter.HistoryMatchHolder holder, int position) {
 
-                if (intent != null) {
+        //get player
+        Match match = mMatchesHistory.get(position);
 
-                    mContext.startActivity(intent);
-                    // overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
-                }
-            }
+        Picasso.with(mContext)
+                .load(match.getClubIdA().getTeam_image())
+                .placeholder(R.drawable.ic_upgraden)
+                .into(holder.teamLogo1);
 
-        });
-        Picasso.with(mContext).
-                load(mClubsMatches.get(pos).getClubIdA().getTeam_image()).
-                placeholder(R.drawable.ic_upgraden).
-                into(holder.teamLogo2);
+        Picasso.with(mContext)
+                .load(match.getClubIdB().getTeam_image())
+                .placeholder(R.drawable.ic_upgraden)
+                .into(holder.teamLogo2);
 
-        // holder.teamLogo2.setImageResource(mClubsMatches.getmWeeklyMatch().get(position).getTeamImage(2));
-        holder.teamLogo2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //calls the Team2 Screen based on the teamID
-                //TODO: replace with local DBSource
-                Intent intent = DrawerSelector.onItemSelected((Activity) mContext, Constants.CLUBS_ACTIVITY_BY_TEAM);
-                intent.putExtra("CLUBID", mClubsMatches.get(pos).getClubIdB().getFirebasekey());
+        holder.score.setText(
+                match.getClubIdA().getGoles()
+                        + " - " +
+                        match.getClubIdB().getGoles());
 
-                if (intent != null) {
+        holder.date.setText(match.getDate());
+        holder.stadium.setText(match.getStadium());
 
-                    mContext.startActivity(intent);
-                    // overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
-                }
-            }
+        holder.teamLogo1.setOnClickListener(this);
+        holder.teamLogo2.setOnClickListener(this);
+    }
 
-        });
-        holder.scoreTeam1.setText( result + " - " + result );
-       // holder.scoreTeam2.setText(String.valueOf(5 + (int) (Math.random() * ((5) + 1))));
-       // holder.teamName1.setText(mClubsMatches.getmWeeklyMatch().get(position).getDate());
-
-        holder.stadium.setText(mClubsMatches.get(position).getStadium());
-
+    @Override
+    public void onClick(View v) {
 
     }
 
-// Provides a reference to the views for each data item
+    public static class HistoryMatchHolder extends RecyclerView.ViewHolder {
 
-
-    public static class ScheduleHolder extends RecyclerView.ViewHolder {
-        /*TextView teamName1;
-        TextView teamName2;*/
+        @BindView(R.id.club_history_image_team1)
         ImageView teamLogo1;
+        @BindView(R.id.club_history_image_team2)
         ImageView teamLogo2;
-        TextView scoreTeam1;
-       // TextView scoreTeam2;
-        TextView time;
-        TextView day;
+        @BindView(R.id.club_history_final_score_txt)
+        TextView score;
+        @BindView(R.id.club_history_day_txt)
+        TextView date;
+        @BindView(R.id.club_history_stadium_txt)
         TextView stadium;
 
-        public ScheduleHolder(View v) {
+        public HistoryMatchHolder(View v) {
             super(v);
-
-
-           /* teamName1 = (TextView) v.findViewById(R.id.history_team1_name);
-            teamName2 = (TextView) v.findViewById(R.id.history_team2_name);*/
-            teamLogo1 = (ImageView) v.findViewById(R.id.club_history_image_team1 );
-            teamLogo2 = (ImageView) v.findViewById(R.id.club_history_image_team2);
-            scoreTeam1 = (TextView) v.findViewById(R.id.club_history_final_score_txt);
-            day = (TextView) v.findViewById(R.id.club_history_day_txt);
-            stadium = (TextView) v.findViewById(R.id.club_history_stadium_txt);
-
+            //butter knife bind
+            ButterKnife.bind(this, v);
         }
     }
 }
